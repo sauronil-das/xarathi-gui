41f5efc95e708c18d7579bd5f1498495f8d029a7
sauronil-das <sauronildas10@gmail.com>
2021-10-20 16:49:51 +0530

    chore(backbone): Created structured files for creating buttons into definations to execute

---
+++ test_drive/button_exec_def.py
@@ -0,0 +1,79 @@
+import os
+
+###############################################################
+
+# PUSHER SCRIPTS BELOW - Pushes User defined CSV Files to Database
+# Aboslute path of the scripts to be edited
+
+def agent_push():
+	os.system("")
+
+
+def fixed_dep_push():
+	os.system("")
+
+
+def loan_push():
+	os.system("")
+
+def mem_push():
+
+	os.system("") 
+
+
+def rd_push():
+	os.system("")
+
+def super_push():
+	os.system("")
+
+
+##############################################################
+
+##############################################################
+
+
+# Cronjobs - Manipulates Data in Database Depending Upon the Purpose
+
+def del_month_pay_after_recon():
+	os.system("")
+
+def fetch_loans_for_coll():
+	os.system("")
+
+def fetch_rd_for_coll():
+	os.system("")
+
+def push_daily_loan_coll_to_db():
+	os.system("")
+
+def push_daily_rd_coll_to_db():
+	os.system("")
+
+def recon_fixed_dep():
+	os.system("")
+
+def recon_loan_pay():
+	os.system("")
+
+def recon_rd_pay():
+	os.system("")
+
+def setup_daily_loan_pay():
+	os.system("")
+
+
+###############################################################
+
+##############################################################
+
+# Clear Old Data For BALAJI MACHINE from LOAN AND RD
+
+def del_output():
+	os.system("")
+
+def del_input():
+	os.system("")
+
+
+
---
+++ test_drive/operator_scripts/list_members.py
@@ -0,0 +1,26 @@
+import sqlalchemy as db
+config = {
+     'host' : '192.168.122.111',
+     'port' : 3306,
+     'user' : 'xaraticli',
+     'password': 'SmellyCatSmellyCat',
+     'database': 'NIDHI_BANK'
+}
+
+def connect():
+    db_user = config.get('user')
+    db_pwd = config.get('password')
+    db_host = config.get('host')
+    db_port = config.get('port')
+    db_name = config.get('database')
+    connection_str = "mysql+pymysql://" + db_user + ":" + db_pwd + "@" + db_host + ":" + str(db_port) + "/" + db_name
+    engine = db.create_engine(connection_str)
+    connection = engine.connect()
+    return connection
+sql_handle = connect()
+
+
+def list_members(sql_handle):
+
+	sql_string = "select * from MEMBERS"
+	rs=sql_handle.execute(sql_string)
